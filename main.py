da4128401106df3dde3f388ff20801f9c8b91665
Moroz-Evgeny <ya.moroz2020@gmail.com>
2025-12-06 16:34:58 +0300

    port

--- main.py
+++ main.py
@@ -40,4 +40,4 @@ async def shutdown_event():
     print("Application shutting down")
  
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, access_log=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True, access_log=True)
